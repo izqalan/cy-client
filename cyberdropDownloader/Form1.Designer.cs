5efd97546afc498a2ee9dfa76eca46ba366adb8a
izqalan <izqalan@gmail.com>
2020-09-14 17:34:08 +0800

    Add project files.

---
+++ cyberdropDownloader/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace cyberdropDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.downloadBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // urlBox
+            // 
+            this.urlBox.AcceptsReturn = true;
+            resources.ApplyResources(this.urlBox, "urlBox");
+            this.urlBox.Name = "urlBox";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // downloadBtn
+            // 
+            resources.ApplyResources(this.downloadBtn, "downloadBtn");
+            this.downloadBtn.Name = "downloadBtn";
+            this.downloadBtn.UseVisualStyleBackColor = true;
+            this.downloadBtn.Click += new System.EventHandler(this.downloadBtn_Click);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.downloadBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.urlBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button downloadBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ cyberdropDownloader/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace cyberdropDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void downloadBtn_Click(object sender, EventArgs e)
+        {
+            var scraper = new CyScraper(urlBox.Text);
+            scraper.Start();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class CyScraper
+    {
+        private string url;
+        public CyScraper(String url)
+        {
+            this.url = url;
+        }
+
+        private string title(HtmlAgilityPack.HtmlDocument htmlDoc)
+        {
+            var title = htmlDoc.DocumentNode.SelectNodes("//div/h1[@id='title']").First().Attributes["title"].Value;
+            return title;
+        }
+
+        public void Start()
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(url);
+            string title = this.title(htmlDoc);
+            Console.WriteLine(title);
+            foreach (HtmlNode link in htmlDoc.DocumentNode.SelectNodes("//a[@class='image'][@href]"))
+            {
+                string url = link.Attributes["href"].Value;
+                
+            }
+        }
+    }
+
+
+}
