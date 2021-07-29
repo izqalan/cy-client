51456fcc2d8cae5977c11e8b31f161602279186e
Bailey <Anequit@gmail.com>
2021-07-29 14:14:59 -0400

    Implemented Exit and Minimize controls

--- src/CyberdropDownloader.Avalonia/ViewModels/MainWindowViewModel.cs
+++ src/CyberdropDownloader.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System.Reactive;
 
 namespace CyberdropDownloader.Avalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private Window _mainWindow;
+
+        public MainWindowViewModel(Window mainWindow)
+        {
+            _mainWindow = mainWindow;
+
+            QuitCommand = ReactiveCommand.Create(CloseWindow);
+            MinimizeCommand = ReactiveCommand.Create(MinimizeWindow);
+        }
+
+        public ReactiveCommand<Unit, Unit> QuitCommand { get; }
+        public ReactiveCommand<Unit, Unit> MinimizeCommand { get; }
+
+        private void CloseWindow() => _mainWindow.Close();
+        private void MinimizeWindow() => _mainWindow.WindowState = WindowState.Minimized;
     }
 }
