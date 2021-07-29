c90eca640c22136204d2279dd2b968129ee6d25b
Bailey <Anequit@gmail.com>
2021-07-29 15:08:05 -0400

    TitleBarViewModel

--- src/CyberdropDownloader.Avalonia/ViewModels/MainWindowViewModel.cs
+++ src/CyberdropDownloader.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,25 +1,12 @@
 ﻿using Avalonia.Controls;
-using ReactiveUI;
-using System.Reactive;
 
 namespace CyberdropDownloader.Avalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private Window _mainWindow;
-
         public MainWindowViewModel(Window mainWindow)
         {
-            _mainWindow = mainWindow;
-
-            QuitCommand = ReactiveCommand.Create(CloseWindow);
-            MinimizeCommand = ReactiveCommand.Create(MinimizeWindow);
+            mainWindow.Find<DockPanel>("TitleBar").DataContext = new TitleBarViewModel(mainWindow);
         }
-
-        public ReactiveCommand<Unit, Unit> QuitCommand { get; }
-        public ReactiveCommand<Unit, Unit> MinimizeCommand { get; }
-
-        private void CloseWindow() => _mainWindow.Close();
-        private void MinimizeWindow() => _mainWindow.WindowState = WindowState.Minimized;
     }
 }
---
+++ src/CyberdropDownloader.Avalonia/ViewModels/TitleBarViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System.Reactive;
+
+namespace CyberdropDownloader.Avalonia.ViewModels
+{
+    public class TitleBarViewModel : ViewModelBase
+    {
+        private Window _mainWindow;
+
+        public TitleBarViewModel(Window mainWindow)
+        {
+            _mainWindow = mainWindow;
+
+            QuitCommand = ReactiveCommand.Create(CloseWindow);
+            MinimizeCommand = ReactiveCommand.Create(MinimizeWindow);
+        }
+
+        public ReactiveCommand<Unit, Unit> QuitCommand { get; }
+        public ReactiveCommand<Unit, Unit> MinimizeCommand { get; }
+
+        private void CloseWindow() => _mainWindow.Close();
+        private void MinimizeWindow() => _mainWindow.WindowState = WindowState.Minimized;
+    }
+}
