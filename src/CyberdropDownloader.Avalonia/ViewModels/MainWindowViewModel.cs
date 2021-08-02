546a2135912e28324bc552f19643549675aa0ea3
Bailey <Anequit@gmail.com>
2021-08-02 02:33:17 -0400

    Update MainWindowViewModel.cs

--- src/CyberdropDownloader.Avalonia/ViewModels/MainWindowViewModel.cs
+++ src/CyberdropDownloader.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -5,17 +5,26 @@ namespace CyberdropDownloader.Avalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private TitleBarViewModel _titleBar = null!;
+        private TitleBarViewModel _titleBarViewModel = null!;
+        private OpenFolderViewModel _openFolderViewModel = null!;
+        private DestinationViewModel _destinationViewModel = null!;
 
         public MainWindowViewModel(Window mainWindow)
         {
-            TitleBar = new TitleBarViewModel(mainWindow, mainWindow.Find<DockPanel>("TitleBar"));
+            TitleBarViewModel = new TitleBarViewModel(mainWindow, mainWindow.Find<DockPanel>("TitleBar"));
+            DestinationViewModel = new DestinationViewModel(mainWindow, mainWindow.Find<TextBox>("Destination"));
         }
 
-        public TitleBarViewModel TitleBar
+        public TitleBarViewModel TitleBarViewModel
         {
-            get => _titleBar;
-            set => this.RaiseAndSetIfChanged(ref _titleBar, value);
+            get => _titleBarViewModel;
+            set => this.RaiseAndSetIfChanged(ref _titleBarViewModel, value);
+        }
+
+        public DestinationViewModel DestinationViewModel
+        {
+            get => _destinationViewModel;
+            set => this.RaiseAndSetIfChanged(ref _destinationViewModel, value);
         }
     }
 }
