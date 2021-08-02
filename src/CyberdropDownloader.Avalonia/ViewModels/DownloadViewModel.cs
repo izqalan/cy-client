f0f97edcdd8d789a6f791847ba7276da7ddd4020
Bailey <Anequit@gmail.com>
2021-08-02 10:18:12 -0400

    Create DownloadViewModel.cs

---
+++ src/CyberdropDownloader.Avalonia/ViewModels/DownloadViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberdropDownloader.Avalonia.ViewModels
+{
+    public class DownloadViewModel : ViewModelBase
+    {
+        public DownloadViewModel()
+        {
+
+        }
+    }
+}
