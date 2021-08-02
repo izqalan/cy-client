4a057735529eb4ac32816b91b99efaf7b9eb495d
Bailey <Anequit@gmail.com>
2021-08-02 02:33:13 -0400

    Create DestinationViewModel.cs

---
+++ src/CyberdropDownloader.Avalonia/ViewModels/DestinationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberdropDownloader.Avalonia.ViewModels
+{
+    public class DestinationViewModel : ViewModelBase
+    {
+        private TextBox _destinationTextBox;
+        private Window _mainWindow;
+
+        public DestinationViewModel(Window mainWindow, TextBox destinationTextbox)
+        {
+            _mainWindow = mainWindow;
+            _destinationTextBox = destinationTextbox;
+
+            _destinationTextBox.PointerReleased += DestinationTextBox_Tapped;
+        }
+
+        private async void DestinationTextBox_Tapped(object? sender, global::Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var dialog = new OpenFolderDialog()
+            {
+                Directory = Environment.CurrentDirectory,
+                Title = "Choose download destination"
+            };
+            
+            _destinationTextBox.Text = await dialog.ShowAsync(_mainWindow);
+        }
+    }
+}
