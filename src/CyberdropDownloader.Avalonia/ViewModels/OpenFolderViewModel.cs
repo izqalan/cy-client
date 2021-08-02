cbcd11838f73f966eb22d7020a3922d68ff5cd6a
Bailey <Anequit@gmail.com>
2021-08-02 10:18:00 -0400

    Update OpenFolderViewModel.cs

--- src/CyberdropDownloader.Avalonia/ViewModels/OpenFolderViewModel.cs
+++ src/CyberdropDownloader.Avalonia/ViewModels/OpenFolderViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia.Controls;
+using System.Diagnostics;
+using System.IO;
+using ReactiveUI;
+using System.Reactive;
 
 namespace CyberdropDownloader.Avalonia.ViewModels
 {
-    class OpenFolderViewModel
+    public class OpenFolderViewModel
     {
+        private TextBox _destinationTextBox;
+
+        public OpenFolderViewModel(TextBox destinationTextBox)
+        {
+            _destinationTextBox = destinationTextBox;
+
+            OpenFolderCommand = ReactiveCommand.Create(OpenFolder);
+        }
+
+        public ReactiveCommand<Unit, Unit> OpenFolderCommand { get; }
+
+        private void OpenFolder()
+        {
+            if (Directory.Exists(_destinationTextBox.Text))
+            {
+                Process.Start("explorer.exe", _destinationTextBox.Text);
+            }
+            else
+            {
+                //TODO Report to log that path isn't real
+            }
+        }
     }
 }
