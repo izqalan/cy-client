28827bb90b5979b6d171a024ebdf70d7710e7734
Bailey <Anequit@gmail.com>
2021-08-02 20:51:22 -0400

    Update DownloadViewModel.cs

--- src/CyberdropDownloader.Avalonia/ViewModels/DownloadViewModel.cs
+++ src/CyberdropDownloader.Avalonia/ViewModels/DownloadViewModel.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive;
 
 namespace CyberdropDownloader.Avalonia.ViewModels
 {
     public class DownloadViewModel : ViewModelBase
     {
-        public DownloadViewModel()
-        {
+        private Label _albumTitle = null!;
+        private TextBlock _downloadLog = null!;
 
+        public DownloadViewModel(Label albumTitle, TextBox urlInput, TextBlock downloadLog)
+        {
+            _albumTitle = albumTitle;
+            _downloadLog = downloadLog;
+
+            DownloadCommand = ReactiveCommand.Create(DownloadFiles);
+        }
+
+        private List<string>? ParseUrlInput(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReactiveCommand<Unit, Unit> DownloadCommand { get; }
+        
+        private void DownloadFiles()
+        {
+            // download files
+            /* if _albumTitle.Text != empty
+             * foreach line in _albumTitle.Text
+             * download line
+             */
         }
     }
 }
