ac7759eaaf67bee88203c8b28b99067b755d2e25
Bailey <Anequit@gmail.com>
2021-08-12 14:15:18 -0400

    Update OpenFolderViewModel.cs

--- src/CyberdropDownloader.Avalonia/ViewModels/OpenFolderViewModel.cs
+++ src/CyberdropDownloader.Avalonia/ViewModels/OpenFolderViewModel.cs
@@ -9,11 +9,13 @@ namespace CyberdropDownloader.Avalonia.ViewModels
 {
     public class OpenFolderViewModel
     {
-        private TextBox _destinationTextBox;
+        private TextBox _destinationTextBox = null!;
+        private TextBlock _downloadLog = null!;
 
         public OpenFolderViewModel(Window mainWindow)
         {
             _destinationTextBox = mainWindow.Find<FolderDestination>("FolderDestination").Find<TextBox>("FolderDestinationTextBox");
+            _downloadLog = mainWindow.Find<DownloadLog>("DownloadLog").Find<TextBlock>("DownloadLogTextBlock");
 
             OpenFolderDestinationCommand = ReactiveCommand.Create(OpenFolder);
         }
