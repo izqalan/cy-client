4c0fec743d94cac236224d90dea8d59d68dcd631
Bailey <Anequit@gmail.com>
2021-08-13 01:50:18 -0400

    Create WebScraperTests.cs

---
+++ src/CyberdropDownloader.Tests/WebScraperTests.cs
@@ -0,0 +1,44 @@
+﻿using CyberdropDownloader.Core;
+using CyberdropDownloader.Core.DataModels;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CyberdropDownloader.Tests
+{
+    public class WebScraperTests
+    {
+        WebScraper webScraper = new WebScraper("https://cyberdrop.me/a/mowoshqo");
+
+        [Fact]
+        public async void LoadPage()
+        {
+            bool result = await webScraper.LoadHtmlDocumenteAsync();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void FetchAlbumTitle()
+        {
+            string result = await webScraper.FetchAlbumTitleAsync();
+
+            Assert.Equal("tests", result);
+        }
+
+        [Fact]
+        public async void FetchAlbumSize()
+        {
+            string result = await webScraper.FetchAlbumSizeAsync();
+
+            Assert.Equal("4.58 MB", result);
+        }
+
+        [Fact]
+        public async void FetchAlbumFiles()
+        {
+            Queue<AlbumFile> result = await webScraper.FetchAlbumFilesAsync();
+
+            Assert.Equal(3, result.Count);
+        }
+    }
+}
