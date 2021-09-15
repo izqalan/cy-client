377d14ada8f2c6eaf7c5df40fb1ebdb4818e127f
Bailey <Anequit@gmail.com>
2021-09-14 23:01:49 -0400

    Create Chunk.cs

---
+++ src/CyberdropDownloader.Core/DataModels/Chunk.cs
@@ -0,0 +1,9 @@
+﻿namespace CyberdropDownloader.Core.DataModels
+{
+    public class Chunk
+    {
+        public long Start { get; set; }
+        public byte[] Data { get; set; }
+        public long End { get; set; }
+    }
+}
