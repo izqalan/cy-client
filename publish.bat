3d185da07879f0b14c5d8e0a47b76dd889883294
Anequit <Anequit@gmail.com>
2021-11-14 09:12:23 -0500

    linux binary hotfix

--- publish.bat
+++ publish.bat
@@ -6,7 +6,7 @@ echo win-x64 published
 dotnet publish .\src\CyberdropDownloader.Avalonia\ -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishProfile=win-x86 -v quiet /p:DebugType=None
 echo win-x86 published
  
-dotnet publish .\src\CyberdropDownloader.Avalonia\ -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishProfile=linux-x64 -v quiet /p:DebugType=None
+dotnet publish .\src\CyberdropDownloader.Avalonia\ -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=false -p:PublishProfile=linux-x64 -v quiet /p:DebugType=None
 echo linux-x64 published
  
 dotnet publish .\src\CyberdropDownloader.Avalonia\ -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishProfile=osx-x64 -v quiet /p:DebugType=None
