54dffa19a0ba7811b1ec9f113ec5d8c0f037330d
Bailey <Anequit@gmail.com>
2022-01-23 18:06:11 -0500

    #61 unknown webscraper error (#73)
    * Update WebScraper.cs
    
    * Update CyberdropDownloader.Avalonia.csproj
    
    * Update MainWindowViewModel.cs
    
    * Update CyberdropDownloader.Avalonia.csproj
    
    * Update Album.cs
    
    * Update CyberdropDownloader.Avalonia.csproj

--- src/CyberdropDownloader.Core/DataModels/Album.cs
+++ src/CyberdropDownloader.Core/DataModels/Album.cs
@@ -1,44 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace CyberdropDownloader.Core.DataModels
 {
-	public class Album
-	{
-		private readonly string _title;
-		private readonly double _size;
-		private readonly Queue<AlbumFile> _files;
+    public class Album
+    {
+        private readonly string _title;
+        private readonly double _size;
+        private readonly Queue<AlbumFile> _files;
 
-		public Album(string title, string size, Queue<AlbumFile> files)
-		{
-			_title = title;
-			_size = ConvertAlbumSizeToBytes(size);
-			_files = files;
-		}
+        public Album(string title, string size, Queue<AlbumFile> files)
+        {
+            _title = title;
+            _size = ConvertAlbumSizeToBytes(size);
+            _files = files;
+        }
 
-		public string Title => _title;
-		public double Size => _size;
-		public Queue<AlbumFile> Files => _files;
+        public string Title => _title;
+        public double Size => _size;
+        public Queue<AlbumFile> Files => _files;
 
-		private double ConvertAlbumSizeToBytes(string albumSize)
-		{
-			decimal byteValue = 0;
+        private double ConvertAlbumSizeToBytes(string albumSize)
+        {
+            // KB, MB, GB, TB
+            string sizeAbbreviation = albumSize.Substring(albumSize.Length - 2);
 
-			if (albumSize.Contains("KB"))
-				byteValue = 1024;
-			else if (albumSize.Contains("MB"))
-				byteValue = 1048576;
-			else if (albumSize.Contains("GB"))
-				byteValue = 1073741824;
-			else if (albumSize.Contains("TB"))
-				byteValue = 1099511627776;
+            decimal albumSizeDecimal = Convert.ToDecimal(albumSize.Replace(sizeAbbreviation, string.Empty).Trim());
 
-			decimal regexValue = Convert.ToDecimal(Regex.Replace(albumSize, @"[a-zA-Z]+", string.Empty));
+            switch(sizeAbbreviation)
+            {
+                case "KB":
+                    albumSizeDecimal *= 1000;
+                    break;
 
-			double roundedValue = Convert.ToDouble(decimal.Round(regexValue * byteValue, 0));
+                case "MB":
+                    albumSizeDecimal *= 1000000;
+                    break;
 
-			return roundedValue;
-		}
-	}
+                case "GB":
+                    albumSizeDecimal *= 1000000000;
+                    break;
+
+                case "TB":
+                    albumSizeDecimal *= 1000000000000;
+                    break;
+
+                default:
+                    throw new Exception("Unknown filesize");
+            }
+
+            double roundedValue = Convert.ToDouble(decimal.Round(albumSizeDecimal, 0));
+
+            return roundedValue;
+        }
+    }
 }
