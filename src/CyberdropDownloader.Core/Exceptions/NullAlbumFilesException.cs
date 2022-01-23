e1bfb0b89fdf6aecca491a13859ad9e0080e7985
Bailey <Anequit@gmail.com>
2022-01-22 21:52:25 -0500

    Added exceptions for webscraper

---
+++ src/CyberdropDownloader.Core/Exceptions/NullAlbumFilesException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CyberdropDownloader.Core.Exceptions
+{
+    public class NullAlbumFilesException : Exception
+    {
+        public NullAlbumFilesException()
+        {
+        }
+
+        public NullAlbumFilesException(string message) : base(message)
+        {
+        }
+
+        public NullAlbumFilesException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NullAlbumFilesException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
